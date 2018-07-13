987cb8e47cd258da68b1d67d196fbe2d6de16da7
shannon <shannon@routific.com>
2018-07-13 14:36:34 -0700

    attempt at google places autocomplete

--- frontend/src/components/SideBar/AddLocationForm.js
+++ frontend/src/components/SideBar/AddLocationForm.js
@@ -1,45 +1,66 @@
 import React, {Component} from 'react';
 import {Button, Col, Form, FormControl, FormGroup, Modal} from "react-bootstrap";
+import {LocationSearchInput} from "./LocationSearchInput";
 
 class AddLocationForm extends Component {
   constructor(props) {
     super(props);
+    this.state = { };
+    this.updateLocation = this.updateLocation.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const name = this.name.value;
-    const lat = this.lat.value;
-    const lng = this.lng.value;
-    const discount = this.discount.value;
-    this.props.addLocation({ name, lat, lng, discount });
+  handleChange(e) {
+    this.setState({name: e.target.value});
+  }
+
+  updateLocation(latLng) {
+    this.setState({lat: latLng.lat, lng: latLng.lng});
+  }
+
+  handleSubmit() {
+    const name = this.state.name;
+    const lat = this.state.lat;
+    const lng = this.state.lng;
+    const discount = this.state.discount;
+    this.props.addLocation({name, lat, lng, discount});
     this.props.handleClose();
-  }
-
-  renderFormGroup(fieldName, inputType, placeholderText, refName) {
-    return (
-      <FormGroup>
-        <Col sm={2}>{fieldName}</Col>
-        <Col sm={10}>
-          <FormControl
-            type={inputType}
-            placeholder={placeholderText}
-            step={inputType === "number" ? 0.000001 : 0}
-            inputRef={ref => {this[refName] = ref}}/>
-        </Col>
-      </FormGroup>
-    );
   }
 
   // TODO: better copy for placeholder text
   render() {
-    return <Form horizontal onSubmit={e => this.handleSubmit(e)}>
-      {this.renderFormGroup("Name", "text", "Enter a location name", "name")}
-      {this.renderFormGroup("Latitude", "number", "Enter a location latitude", "lat")}
-      {this.renderFormGroup("Longitude", "number", "Enter a location longitude", "lng")}
-      {this.renderFormGroup("Discount", "number", "Enter a location discount", "discount")}
+    return <Form horizontal>
+      <FormGroup>
+        <Col sm={2}>Name</Col>
+        <Col sm={10}>
+          <FormControl
+            type="text"
+            placeholder="Enter a location name"
+            value={this.state.name}
+            onChange={this.handleChange.bind(this)}/>
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col sm={2}>Discount</Col>
+        <Col sm={10}>
+          <FormControl
+            type="number"
+            placeholder="Enter a location discount"
+            step={0.01}
+            value={this.state.discount}
+            onChange={this.handleChange.bind(this)}/>
+        </Col>
+      </FormGroup>
+
+
+      <FormGroup>
+        <Col sm={2}>Location</Col>
+        <Col sm={10}>
+          <LocationSearchInput updateLocation={this.updateLocation}/>
+        </Col>
+      </FormGroup>
       <Modal.Footer>
-        <Button bsStyle="primary" type="submit">Submit</Button>
+        <Button bsStyle="primary" onClick={() => this.handleSubmit()}>Submit</Button>
       </Modal.Footer>
     </Form>
   }
